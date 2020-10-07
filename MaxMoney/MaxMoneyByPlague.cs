b0a710f257974be4e3bbd3d05fed48994cb3a01e
Plague <36628963+OFWModz@users.noreply.github.com>
2020-10-07 02:20:28 +0100

    Add files via upload

---
+++ MaxMoney/MaxMoneyByPlague.cs
@@ -0,0 +1,19 @@
+﻿using MelonLoader;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace MaxMoney
+{
+    public class MaxMoneyByPlague : MelonMod
+    {
+        public override void OnGUI()
+        {
+            if (GUI.Button(new Rect(10, 35, 150, 25), "Max Money: " + FileBasedPrefs.GetInt("PlayersMoney")))
+            {
+                FileBasedPrefs.SetInt("PlayersMoney", 999999);
+
+                FileBasedPrefs.ManualySave();
+            }
+        }
+    }
+}
